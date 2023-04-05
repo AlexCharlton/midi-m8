b9d1ca06eb6a0e1c92321efea73309e9b2f6a375
Alex Charlton <alex.n.charlton@gmail.com>
2023-04-04 19:24:34 -0700

    Move lemna_nih_plug to lemna

--- plugin/src/lib.rs
+++ plugin/src/lib.rs
@@ -1,7 +1,52 @@
+use lemna::{self, widgets, *};
+use lemna_nih_plug::nih_plug;
 use nih_plug::prelude::*;
 use std::sync::Arc;
 
-mod nih_plug_lemna;
+type Renderer = lemna::render::wgpu::WGPURenderer;
+type Node = lemna::Node<Renderer>;
+
+#[derive(Debug)]
+pub struct HelloApp {}
+
+impl lemna::Component<Renderer> for HelloApp {
+    fn view(&self) -> Option<Node> {
+        Some(
+            node!(
+                widgets::Div::new(),
+                lay!(size: size_pct!(100.0), wrap: true,
+                     padding: rect!(10.0),
+                     axis_alignment: Alignment::Center, cross_alignment: Alignment::Center)
+            )
+            .push(node!(
+                widgets::Div::new().bg(Color::rgb(1.0, 0.0, 0.0)),
+                lay!(size: size!(200.0, 100.0), margin: rect!(5.0)),
+                0
+            ))
+            .push(node!(
+                widgets::Div::new().bg(Color::rgb(0.0, 1.0, 0.0)),
+                lay!(size: size!(100.0), margin: rect!(5.0)),
+                1
+            ))
+            .push(node!(
+                widgets::RoundedRect {
+                    background_color: [0.0, 0.0, 1.0].into(),
+                    border_width: 1.0,
+                    ..Default::default()
+                }
+                .radius(5.0),
+                lay!(size: size!(100.0), margin: rect!(5.0)),
+                2
+            )),
+        )
+    }
+}
+
+impl lemna::App<Renderer> for HelloApp {
+    fn new() -> Self {
+        Self {}
+    }
+}
 
 #[derive(Default)]
 struct M8Plug {
@@ -38,7 +83,7 @@ impl Plugin for M8Plug {
     }
 
     fn editor(&self, _async_executor: AsyncExecutor<Self>) -> Option<Box<dyn Editor>> {
-        nih_plug_lemna::create_lemna_editor()
+        lemna_nih_plug::create_lemna_editor::<Renderer, HelloApp>("Midi M8", 200, 200, vec![])
     }
 }
 
