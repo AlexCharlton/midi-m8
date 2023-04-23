57ea077e74e9a56fcc764ed6b49640e3a95726e7
Alex Charlton <alex.n.charlton@gmail.com>
2023-04-22 22:32:46 -0700

    dnd plugin

--- plugin/readme.md
+++ plugin/readme.md
@@ -1,6 +1,12 @@
-Running
+## Running
 
+Standalone
 ```
+$ cargo xtask bundle -p midi-m8-plugin && NIH_LOG=./plugin.log ./target/debug/midi-m8-plugin[.exe]
 $ cargo xtask bundle -p midi-m8-plugin
-$ ./target/debug/midi-m8-plugin[.exe]
+```
+
+Plugin
+```
+$ cargo xtask bundle -p midi-m8-plugin && cp ./target/bundled/midi-m8-plugin.clap /c/Program\ Files/Common\ Files/CLAP/
 ```
--- plugin/src/lib.rs
+++ plugin/src/lib.rs
@@ -6,10 +6,125 @@ use std::sync::Arc;
 type Renderer = lemna::render::wgpu::WGPURenderer;
 type Node = lemna::Node<Renderer>;
 
-#[derive(Debug)]
-pub struct HelloApp {}
+#[derive(Debug, Default)]
+pub struct DropTargetState {
+    active: bool,
+}
 
-impl lemna::Component<Renderer> for HelloApp {
+#[state_component(DropTargetState)]
+#[derive(Debug)]
+pub struct DropTarget {}
+
+impl DropTarget {
+    fn new() -> Self {
+        Self {
+            state: Some(DropTargetState::default()),
+        }
+    }
+}
+
+#[state_component_impl(DropTargetState)]
+impl Component<Renderer> for DropTarget {
+    fn view(&self) -> Option<Node> {
+        Some(
+            node!(
+                widgets::Div::new()
+                    .bg(if self.state_ref().active {
+                        Color::rgb(1.0, 0.5, 0.5)
+                    } else {
+                        Color::rgb(0.5, 1.0, 0.5)
+                    })
+                    .border(Color::BLACK, 2.0),
+                lay!(
+                    size: size_pct!(100.0),
+                    margin: rect!(10.0),
+                    padding: rect!(5.0),
+                    cross_alignment: crate::layout::Alignment::Center,
+                    axis_alignment: crate::layout::Alignment::Center
+                ),
+                0
+            )
+            .push(node!(widgets::Text::new(
+                txt!("Drag something onto me"),
+                widgets::TextStyle {
+                    h_alignment: HorizontalAlign::Center,
+                    ..widgets::TextStyle::default()
+                }
+            ))),
+        )
+    }
+
+    fn on_drag_drop(&mut self, event: &mut Event<event::DragDrop>) -> Vec<Message> {
+        println!("Got {:?}", event.input.0);
+        self.state_mut().active = false;
+        event.dirty();
+        vec![]
+    }
+
+    fn on_drag_enter(&mut self, event: &mut Event<event::DragEnter>) -> Vec<Message> {
+        self.state_mut().active = true;
+        current_window().unwrap().set_drop_target_valid(true);
+        event.dirty();
+        vec![]
+    }
+
+    fn on_drag_leave(&mut self, event: &mut Event<event::DragLeave>) -> Vec<Message> {
+        self.state_mut().active = false;
+        current_window().unwrap().set_drop_target_valid(false);
+        event.dirty();
+        vec![]
+    }
+
+    fn on_drag_target(&mut self, event: &mut Event<event::DragTarget>) -> Vec<Message> {
+        event.stop_bubbling();
+        vec![]
+    }
+}
+
+#[derive(Debug)]
+pub struct DragSource {}
+
+impl Component<Renderer> for DragSource {
+    fn view(&self) -> Option<Node> {
+        Some(
+            node!(
+                widgets::Div::new()
+                    .bg(Color::rgb(0.5, 0.5, 1.0))
+                    .border(Color::BLACK, 2.0),
+                lay!(
+                    size: size_pct!(100.0),
+                    margin: rect!(10.0),
+                    padding: rect!(5.0),
+                    cross_alignment: crate::layout::Alignment::Center,
+                    axis_alignment: crate::layout::Alignment::Center
+                ),
+                0
+            )
+            .push(node!(widgets::Text::new(
+                txt!("Drag from me"),
+                widgets::TextStyle {
+                    h_alignment: HorizontalAlign::Center,
+                    ..widgets::TextStyle::default()
+                }
+            ))),
+        )
+    }
+
+    fn on_drag_start(&mut self, event: &mut Event<event::DragStart>) -> Vec<Message> {
+        current_window()
+            .unwrap()
+            .start_drag(Data::Filepath("/test/file.txt".into()));
+        event.stop_bubbling();
+        vec![]
+    }
+}
+
+/// App
+
+#[derive(Debug)]
+pub struct M8PlugApp {}
+
+impl lemna::Component<Renderer> for M8PlugApp {
     fn view(&self) -> Option<Node> {
         Some(
             node!(
@@ -18,31 +133,24 @@ impl lemna::Component<Renderer> for HelloApp {
                      padding: rect!(10.0),
                      axis_alignment: Alignment::Center, cross_alignment: Alignment::Center)
             )
-            .push(node!(
-                widgets::Div::new().bg(Color::rgb(1.0, 0.0, 0.0)),
-                lay!(size: size!(200.0, 100.0), margin: rect!(5.0)),
-                0
-            ))
-            .push(node!(
-                widgets::Div::new().bg(Color::rgb(0.0, 1.0, 0.0)),
-                lay!(size: size!(100.0), margin: rect!(5.0)),
-                1
-            ))
-            .push(node!(
-                widgets::RoundedRect {
-                    background_color: [0.0, 0.0, 1.0].into(),
-                    border_width: 1.0,
-                    ..Default::default()
-                }
-                .radius(5.0),
-                lay!(size: size!(100.0), margin: rect!(5.0)),
-                2
-            )),
+            .push(node!(DropTarget::new(), lay!(size: size!(100.0)), 0))
+            .push(node!(DragSource {}, lay!(size: size!(100.0)), 0)),
         )
+    }
+
+    fn on_drag_drop(&mut self, event: &mut Event<event::DragDrop>) -> Vec<Message> {
+        // This will never print, because this is not a valid target per `on_drag_target`
+        println!("Oops, you missed the target. Got {:?}", event.input.0);
+        vec![]
+    }
+
+    fn on_drag_target(&mut self, _event: &mut Event<event::DragTarget>) -> Vec<Message> {
+        current_window().unwrap().set_drop_target_valid(false);
+        vec![]
     }
 }
 
-impl lemna::App<Renderer> for HelloApp {
+impl lemna::App<Renderer> for M8PlugApp {
     fn new() -> Self {
         Self {}
     }
@@ -83,7 +191,12 @@ impl Plugin for M8Plug {
     }
 
     fn editor(&self, _async_executor: AsyncExecutor<Self>) -> Option<Box<dyn Editor>> {
-        lemna_nih_plug::create_lemna_editor::<Renderer, HelloApp>("Midi M8", 200, 200, vec![])
+        lemna_nih_plug::create_lemna_editor::<Renderer, M8PlugApp>(
+            "Midi M8",
+            400,
+            300,
+            vec![("noto sans regular".to_string(), ttf_noto_sans::REGULAR)],
+        )
     }
 }
 
