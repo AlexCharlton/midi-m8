1beef3cbf764407a8d74d5f40b7475f82847bc88
Alex Charlton <alex.charlton@instacart.com>
2023-05-05 16:55:26 -0700

    Fix build script for non-windows

--- plugin/build.rs
+++ plugin/build.rs
@@ -1,10 +1,10 @@
-extern crate winres;
-
+#[cfg(target_os = "windows")]
 fn main() {
-    if cfg!(target_os = "windows") {
-        let mut res = winres::WindowsResource::new();
-        res.set_icon("./include/256.ico");
-        res.compile().unwrap();
-        println!("Compiled resource into exe");
-    }
+    let mut res = winres::WindowsResource::new();
+    res.set_icon("./include/256.ico");
+    res.compile().unwrap();
+    println!("Compiled resource into exe");
 }
+
+#[cfg(not(target_os = "windows"))]
+fn main() {}
